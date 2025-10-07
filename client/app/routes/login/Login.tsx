2d649e86c9e119587c49280fd3642f69f5b89d8a
Anass Moussadi <a.moussadi03@gmail.com>
2025-10-07 13:34:06 +0200

    frontend login adadwada

--- client/app/routes/login/Login.tsx
+++ client/app/routes/login/Login.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "../../+types/root";
 import styles from "./login.module.css";
-import { Form } from "react-router";
+import { Form, redirect, type ActionFunctionArgs } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,10 +9,29 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+export async function action({ request }: ActionFunctionArgs) {
+  const loginCreds = await request.formData();
+  const username = loginCreds.get("username") as string;
+  const password = loginCreds.get("password") as string;
+
+  const response = await fetch("http://localhost:5175/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+    credentials: "include",
+  });
+
+  if (response.ok) {
+    return redirect("/dashboard");
+  } else {
+    return { error: (await response.text()) || "Invalid username or password" };
+  }
+}
+
 export default function Login() {
   return (
     <div className={styles.container}>
-      <Form className={styles.login} action="/dashboard" method="post">
+      <Form className={styles.login} method="post">
         <h2>Login</h2>
         <input name="username" type="text" placeholder="Dikke boktor" />
         <input name="password" type="password" placeholder="password123" />
