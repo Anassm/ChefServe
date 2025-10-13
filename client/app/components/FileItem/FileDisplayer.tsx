0533f6fd653a6f4f3a630749f6bd60099a77b23f
jens koning <jens.koning@icloud.com>
2025-10-13 12:25:48 +0200

    werkt nog niet maar ga later verder

---
+++ client/app/components/FileItem/FileDisplayer.tsx
@@ -0,0 +1,5 @@
+import styles from './FileItem.module.css';
+
+export function FileDisplayer() {
+    return 
+}
--- client/app/components/FileItem/FileItem.tsx
+++ client/app/components/FileItem/FileItem.tsx
@@ -1,5 +1,5 @@
 import styles from './FileItem.module.css';
-type Props = {
+export type Props = {
     id: string;
     name: string;
     extension: string;
@@ -20,22 +20,3 @@ export function FileItem({ id, name, extension, isFolder }: Props) {
 }
 
 
-
-// export function FileItem({ id, name, isFolder }: Props) {
-
-//     var object = 
-//     <div className="file-item-wrapper">
-//         <div className="icon">
-//             <button className="imageButton"><img src="/images/txt.webp" width="64" height="64"alt="ALT"/></button>
-//         </div>
-//         <div className="name">
-//             <p>{name}</p>
-//         </div>
-
-//     </div>;
-
-//     return object;
-// }
-
-// C:\Users\jensk\Desktop\HR\J2S2\Web Dev\ChefServe\client\app\components\FileItem\images\txt.webp
-// C:\Users\jensk\Desktop\HR\J2S2\Web Dev\ChefServe\client\app\components\FileItem\FileItem.tsx
