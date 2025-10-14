d8e74650046be7bf2fe9221df32e970a2ca5c71e
jenskon <jens.koning@icloud.com>
2025-10-14 15:57:21 +0200

    alleen nog backend cookie fix doen

--- client/app/components/FileItem/FileDisplayer.tsx
+++ client/app/components/FileItem/FileDisplayer.tsx
@@ -2,9 +2,7 @@ import styles from './FileItem.module.css';
 import type { fileItem } from './FileItem';
 import { FileItem } from './FileItem';
 
-export function FileDisplayer(props : fileItem[]) {
-    const displayItems = props.map(Item => <FileItem name={Item.name} extension={Item.extension} isFolder={Item.isFolder} hasContent={Item.hasContent} />)
+export default function FileDisplayer({ files }: { files: fileItem[] }) {
+    const displayItems = files.map(Item => <FileItem name={Item.name} extension={Item.extension} isFolder={Item.isFolder} hasContent={Item.hasContent} />)
     return (displayItems);
 }
-
-export default FileDisplayer;
