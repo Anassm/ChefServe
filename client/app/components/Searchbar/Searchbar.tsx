726e460132af58e58a966ccd4bf14aebc8ecadba
Anass Moussadi <a.moussadi03@gmail.com>
2025-11-04 14:50:34 +0100

    frontend progressie, new icons and header/sidebar height change

---
+++ client/app/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,16 @@
+import { IoIosSearch } from "react-icons/io";
+
+export default function Searchbar() {
+    function handleChange() {
+        return 
+    }
+
+  return (
+    <>
+      <input type="text" onChange={} />
+      <button>
+        <IoIosSearch />
+      </button>
+    </>
+  );
+}
