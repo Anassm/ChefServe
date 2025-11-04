56e19190208d6727d84769ea751722bb08cc9d6e
Anass Moussadi <a.moussadi03@gmail.com>
2025-11-04 12:53:29 +0100

    xxx

--- client/app/components/MainLayout/Header/Header.tsx
+++ client/app/components/MainLayout/Header/Header.tsx
@@ -1,13 +1,114 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import styles from "../MainLayout.module.css";
 import { selectedFileContext } from "~/context/SelectedFileContext";
+import { TiDocumentDelete } from "react-icons/ti";
+import { useRevalidator } from "react-router";
+import { IoMdAdd } from "react-icons/io";
 
 export default function Header() {
-  const selectedFile = useContext(selectedFileContext)
+  const context = useContext(selectedFileContext);
+  if (!context) return null;
+  const { selectedFile, setSelectedFile } = context;
+
+  const revalidator = useRevalidator();
+  const uploadRef = useRef<HTMLInputElement>(null);
+
+  async function onFileDelete() {
+    if (!selectedFile) {
+      alert("No file selected");
+      return;
+    }
+
+    const confirmDelete = confirm(`Delete file "${selectedFile.name}"?`);
+    if (!confirmDelete) return;
+
+    try {
+      const response = await fetch(
+        `http://localhost:5175/api/File/DeleteFile?fileID=${selectedFile.id}`,
+        {
+          method: "DELETE",
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error((await response.text()) || "Failed to delete file");
+      }
+
+      setSelectedFile(null);
+
+      revalidator.revalidate();
+    } catch (err) {
+      console.error(err);
+      alert("Failed to delete file");
+    }
+  }
+
+  async function onUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+    if (!files || files.length === 0) return;
+
+    const file = files[0];
+
+    const confirmUpload = confirm(`Upload file "${file.name}"?`);
+    if (!confirmUpload) return;
+
+    const formData = new FormData();
+    formData.append("FileName", file.name);
+    formData.append("Content", file);
+    formData.append("DestinationPath", "/"); // Voor nu even "/"
+
+    try {
+      const response = await fetch(
+        `http://localhost:5175/api/File/UploadFile`,
+        {
+          method: "POST",
+          body: formData,
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error((await response.text()) || "Failed to upload file");
+      }
+
+      revalidator.revalidate();
+    } catch (err) {
+      console.error(err);
+      alert("Failed to upload file");
+    }
+  }
+
   return (
     <header className={styles.header}>
-      <span>This is a header</span>
-      <button onClick={() => alert(selectedFile ? JSON.stringify(selectedFile, null, 2) : "No file selected")}>Test</button>
+      <div className={styles.fileActions}>
+        <TiDocumentDelete
+          size={40}
+          onClick={!selectedFile ? () => {} : onFileDelete}
+          opacity={!selectedFile ? 0.5 : 1}
+          style={
+            !selectedFile ? { cursor: "not-allowed" } : { cursor: "pointer" }
+          }
+        />
+        <IoMdAdd
+          size={40}
+          onClick={() => uploadRef.current?.click()}
+          style={{ cursor: "pointer" }}
+        />
+        <input
+          ref={uploadRef}
+          type="file"
+          multiple
+          style={{ display: "none" }}
+          onChange={onUpload}
+        />
+      </div>
     </header>
   );
 }
