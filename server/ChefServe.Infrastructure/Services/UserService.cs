2391d3691361195b860c8147f8b3df4e1c096586
RedRoan04 <roanvrossum@gmail.com>
2025-11-07 14:12:21 +0100

    Added create, delete and update user to the backend

--- server/ChefServe.Infrastructure/Services/UserService.cs
+++ server/ChefServe.Infrastructure/Services/UserService.cs
@@ -23,4 +23,32 @@ public class UserService : IUserService
     {
         return await _context.Users.ToListAsync();
     }
+
+    public async Task<bool> DeleteUserAsync(Guid userId)
+    {
+        var user = await _context.Users.FindAsync(userId);
+        if (user == null)
+        {
+            return false;
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<User> CreateUserAsync(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User> UpdateUserAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
 }
