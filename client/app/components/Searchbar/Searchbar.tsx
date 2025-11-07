36e7f9fe0e4ecdec26db700b21fac92149ee8177
Anass Moussadi <a.moussadi03@gmail.com>
2025-11-07 15:09:44 +0100

    searchbar basic styling

--- client/app/components/Searchbar/Searchbar.tsx
+++ client/app/components/Searchbar/Searchbar.tsx
@@ -1,16 +1,36 @@
 import { IoIosSearch } from "react-icons/io";
+import { useSearchParams } from "react-router";
+import styles from "./Searchbar.module.css";
 
 export default function Searchbar() {
-    function handleChange() {
-        return 
+  const [searchInput, setSearchInput] = useSearchParams();
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const search = event.target.value;
+
+    const newSearchInput = new URLSearchParams(searchInput);
+
+    if (search) {
+      newSearchInput.set("q", search);
+    } else {
+      newSearchInput.delete("q");
     }
 
+    setSearchInput(newSearchInput);
+  }
+
+  const search = searchInput.get("q") || "";
+
   return (
-    <>
-      <input type="text" onChange={} />
-      <button>
-        <IoIosSearch />
-      </button>
-    </>
+    <div className={styles.searchbarContainer}>
+      <IoIosSearch size={24} className={styles.icon} />
+      <input
+        className={styles.searchbar}
+        type="text"
+        placeholder={`Search...`}
+        value={search}
+        onChange={handleChange}
+      />
+    </div>
   );
 }
