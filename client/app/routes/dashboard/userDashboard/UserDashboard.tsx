ceb415264e6314050b7215546354153786a5be1b
jenskon <jens.koning@icloud.com>
2025-11-11 09:38:21 +0100

    fix

--- client/app/routes/dashboard/userDashboard/UserDashboard.tsx
+++ client/app/routes/dashboard/userDashboard/UserDashboard.tsx
@@ -2,6 +2,7 @@ import FileDisplayer from "~/components/FileItem/FileDisplayer";
 import type { LoaderFunctionArgs } from "react-router";
 import { useState } from "react";
 import type { TreeItem } from "~/components/FileTree/FileTree";
+import type { fileItem } from "~/components/FileItem/FileItem";
 
 
 
