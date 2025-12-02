945e94815674d9e18060ca39082a88d184f4095a
Anass Moussadi <a.moussadi03@gmail.com>
2025-12-02 14:31:34 +0100

    Create folder implemented

--- client/app/components/MainLayout/Header/Header.tsx
+++ client/app/components/MainLayout/Header/Header.tsx
@@ -1,10 +1,12 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import styles from "../MainLayout.module.css";
 import { selectedFileContext } from "~/context/SelectedFileContext";
 import { TiDocumentDelete } from "react-icons/ti";
 import { useRevalidator } from "react-router";
-import { IoMdAdd } from "react-icons/io";
+import { AiOutlineFileAdd, AiOutlineFolderAdd } from "react-icons/ai";
 import Searchbar from "~/components/Searchbar/Searchbar";
+import BaseModal from "~/components/BaseModal/BaseModal";
+import TextInput from "~/components/TextInput/TextInput";
 
 export default function Header() {
   const context = useContext(selectedFileContext);
@@ -13,6 +15,9 @@ export default function Header() {
 
   const revalidator = useRevalidator();
   const uploadRef = useRef<HTMLInputElement>(null);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newFolderName, setNewFolderName] = useState("");
 
   async function onFileDelete() {
     if (!selectedFile) {
@@ -52,14 +57,13 @@ export default function Header() {
     const files = event.target.files;
     if (!files || files.length === 0) return;
 
-    const file = files[0];
-
-    const confirmUpload = confirm(`Upload file "${file.name}"?`);
-    if (!confirmUpload) return;
-
     const formData = new FormData();
-    formData.append("FileName", file.name);
-    formData.append("Content", file);
+
+    for (const file of Array.from(files)) {
+      formData.append("files", file);
+      formData.append("paths", file.webkitRelativePath || file.name);
+    }
+
     formData.append("DestinationPath", "/"); // Voor nu even "/"
 
     try {
@@ -76,24 +80,57 @@ export default function Header() {
       );
 
       if (!response.ok) {
-        throw new Error((await response.text()) || "Failed to upload file");
+        throw new Error((await response.text()) || "Failed to upload file(s)");
       }
 
       revalidator.revalidate();
     } catch (err) {
       console.error(err);
-      alert("Failed to upload file");
+      alert("Failed to upload file(s)");
     }
   }
 
   async function onCreateFolder() {
-    
+    const currentPath = window.location.pathname || "/";
+
+    const folderData = {
+      folderName: newFolderName,
+      parentPath: currentPath,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:5175/api/File/CreateFolder",
+        {
+          method: "POST",
+          body: JSON.stringify(folderData),
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error((await response.text()) || "Failed to create folder");
+      }
+
+      setNewFolderName("");
+      setIsModalOpen(false);
+      revalidator.revalidate();
+    } catch (err) {
+      console.error(err);
+      alert("Failed to create folder");
+    }
   }
 
   return (
     <header className={styles.header}>
       <div className={styles.fileActions}>
         <Searchbar />
+
+        {/* Delete file */}
         <TiDocumentDelete
           size={40}
           onClick={!selectedFile ? () => {} : onFileDelete}
@@ -102,7 +139,16 @@ export default function Header() {
             !selectedFile ? { cursor: "not-allowed" } : { cursor: "pointer" }
           }
         />
-        <IoMdAdd
+
+        {/* Create folder */}
+        <AiOutlineFolderAdd
+          size={40}
+          onClick={() => setIsModalOpen(true)}
+          style={{ cursor: "pointer" }}
+        />
+
+        {/* Upload file */}
+        <AiOutlineFileAdd
           size={40}
           onClick={() => uploadRef.current?.click()}
           style={{ cursor: "pointer" }}
@@ -111,10 +157,42 @@ export default function Header() {
           ref={uploadRef}
           type="file"
           multiple
+          webkitdirectory="true"
+          directory="true"
           style={{ display: "none" }}
           onChange={onUpload}
         />
       </div>
+
+      {isModalOpen && (
+        <BaseModal title="Create folder" onClose={() => setIsModalOpen(false)}>
+          <TextInput
+            label="Folder name"
+            placeholder="Homework"
+            value={newFolderName}
+            onChange={(e) => {
+              setNewFolderName(e.target.value);
+            }}
+          />
+          <div className={styles.modalButtons}>
+            <button
+              className={styles.submitButton}
+              type="button"
+              onClick={onCreateFolder}
+            >
+              Create
+            </button>
+            <button
+              className={styles.cancelButton}
+              onClick={() => {
+                setIsModalOpen(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </BaseModal>
+      )}
     </header>
   );
 }
