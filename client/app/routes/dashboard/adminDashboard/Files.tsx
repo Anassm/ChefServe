7c318fb7287e5949310fddd0ba2f5c0922c05fc2
RedRoan04 <roanvrossum@gmail.com>
2026-01-16 13:39:50 +0100

    small fix

--- client/app/routes/dashboard/adminDashboard/Files.tsx
+++ client/app/routes/dashboard/adminDashboard/Files.tsx
@@ -9,6 +9,7 @@ import FileTypeStats from "~/components/AdminDashboard/Files/FileTypeStats";
 import TotalStorageCount from "~/components/AdminDashboard/Files/TotalStorageCount";
 import { NavLink, useLoaderData } from "react-router";
 import FileTable from "~/components/AdminDashboard/Files/FileTable";
+import { useState } from "react";
 
 export async function clientLoader({ request }: Route.LoaderArgs) {
     const response = await fetch("http://localhost:5175/api/admin/files", {
@@ -34,6 +35,16 @@ export function HydrateFallback() {
 export default function Files() {
     const loaderData = useLoaderData() as any;
     const filesFromLoader: any[] = loaderData?.data ?? loaderData?.returnData ?? loaderData ?? [];
+    const [files, setFiles] = useState(filesFromLoader || []);
+
+    const refreshFiles = async () => {
+        const response = await fetch("http://localhost:5175/api/admin/files", {
+            credentials: "include",
+        });
+        const updated = await response.json();
+        const filesdata = updated?.data ?? updated?.returnData ?? updated ?? [];
+        setFiles(filesdata);
+    }
 
     return <div style={{overflowY: "auto"}}>
         <div style={{ height: "20px" }}></div>
@@ -58,7 +69,7 @@ export default function Files() {
         <div style={{ height: "20px" }}></div>
         <h1 className={styles.title}>All files</h1>
         <div style={{ height: "20px" }}></div>
-        <FileTable files={filesFromLoader} />
+        <FileTable files={files} onFileDeleted={refreshFiles} />
         <div style={{ height: "50px" }}></div>
     </div>;
 
